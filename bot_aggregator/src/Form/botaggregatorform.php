cf1664760dd60bc024569c50c741aedf898820e1
Heervesh <heerveshdrc1234@gmail.com>
2017-01-07 21:18:25 +0400

    Add files via upload

---
+++ bot_aggregator/src/Form/botaggregatorform.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+*@file
+*contains Drupal\modules\bot\bot_aggregator\Form\botaggregatorform
+*/
+
+namespace Drupal\bot\Form;
+
+use Drupal/Core/Form\ConfigFormBase;
+
+class botaggregatorform extents ConfigFormBase {
+
+ /**
+ *{@inheridoc}
+ */
+ public function getFormId() {
+  return 'bot_aggregator_form';
+ }
+}
