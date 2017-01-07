d6753ced61b4d650b8b6bf47dcff9229ca5b2242
Heervesh <heerveshdrc1234@gmail.com>
2017-01-07 21:23:30 +0400

    Add files via upload

---
+++ bot_factoid/src/Form/botfactoidform.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+*@file
+*contains Drupal\modules\bot\bot_factoid \Form\botfactoidform
+*/
+
+namespace Drupal\bot\bot_factoid\Form;
+
+use Drupal/Core/Form/ConfigFormBase;
+
+class botfactoidform extents ConfigFormBase {
+
+ /**
+ *{@inheridoc}
+ */
+ public function getFormId() {
+  return 'bot_factoid_form';
+ }
+}
