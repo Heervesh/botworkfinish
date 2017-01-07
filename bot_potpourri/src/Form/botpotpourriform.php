ce7dc68f9c95aebc0ec303a2568c62de812b4f1c
Heervesh <heerveshdrc1234@gmail.com>
2017-01-07 21:26:51 +0400

    Add files via upload

---
+++ bot_potpourri/src/Form/botpotpourriform.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+*@file
+*contains Drupal\modules\bot\bot_potpourri\Form\botpotpourriform
+*/
+
+namespace Drupal\bot\bot_potpourri\Form;
+
+use Drupal/Core/Form/ConfigFormBase;
+
+class botpotpourriform extents ConfigFormBase {
+
+ /**
+ *{@inheridoc}
+ */
+ public function getFormId() {
+  return 'bot_potpourri_form';
+ }
+}
