b913fc370f78a577e19c45d525d693570d674e47
Heervesh <heerveshdrc1234@gmail.com>
2017-01-07 21:20:48 +0400

    Add files via upload

---
+++ bot_agotchi/src/Form/botagotchiform.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+*@file
+*contains Drupal\modules\bot_agotchi\Form\botagotchiform
+*/
+
+namespace Drupal\modules\bot_agotchi\Form;
+
+use Drupal/Core/Form\ConfigFormBase;
+
+class  botagotchiform extents ConfigFormBase {
+
+ /**
+ *{@inheridoc}
+ */
+ public function getFormId() {
+  return 'bot_agotchi_form';
+ }
+}
