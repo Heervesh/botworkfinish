5d043c0a181213032cf33e5c9073ab4a3e49731f
Heervesh <heerveshdrc1234@gmail.com>
2017-01-07 21:29:30 +0400

    Add files via upload

---
+++ bot_tell/src/form/bottellform.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+*@file
+*contains Drupal\modules\bot\bot_tell\Form\bottellform
+*/
+
+namespace Drupal\bot\bot_tell\Form;
+
+use Drupal/Core/Form/ConfigFormBase;
+
+class bottellform extents ConfigFormBase {
+
+ /**
+ *{@inheridoc}
+ */
+ public function getFormId() {
+  return 'bot_tell_form';
+ }
+}
