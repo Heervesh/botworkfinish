2e3d51be7a6707ee33691f1f4a1f313f0e2be1a8
Heervesh <heerveshdrc1234@gmail.com>
2017-01-07 21:24:45 +0400

    Add files via upload

---
+++ bot_karma/src/Form/botkarmaform.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+*@file
+*contains Drupal\modules\bot\bot_karma\Form\botkarmaform
+*/
+
+namespace Drupal\bot\bot_karma\Form;
+
+use Drupal/Core/Form/ConfigFormBase;
+
+class botkarma form extents ConfigFormBase {
+
+ /**
+ *{@inheridoc}
+ */
+ public function getFormId() {
+  return 'bot_karma_form';
+ }
+}
