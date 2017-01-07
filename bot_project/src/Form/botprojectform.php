0681a3bb1bf9d8e7c708d0a0ce4ff379e899cb26
Heervesh <heerveshdrc1234@gmail.com>
2017-01-07 21:27:22 +0400

    Add files via upload

---
+++ bot_project/src/Form/botprojectform.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+*@file
+*contains Drupal\modules\bot\bot_project\Form\botprojectform
+*/
+
+namespace Drupal\bot\bot_project\Form;
+
+use Drupal/Core/Form/ConfigFormBase;
+
+class botprojectform extents ConfigFormBase {
+
+ /**
+ *{@inheridoc}
+ */
+ public function getFormId() {
+  return 'bot_project_form';
+ }
+}
