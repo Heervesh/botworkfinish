6886386b2ba190c82acb44b3a1bcb7111a84829b
Heervesh <heerveshdrc1234@gmail.com>
2017-01-07 21:26:14 +0400

    Add files via upload

---
+++ bot_log/src/Form/botlogform.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+*@file
+*contains Drupal\modules\bot\bot_log\Form\botlogform
+*/
+
+namespace Drupal\bot\bot_log\Form;
+
+use Drupal/Core/Form/ConfigFormBase;
+
+class botlogform extents ConfigFormBase {
+
+ /**
+ *{@inheridoc}
+ */
+ public function getFormId() {
+  return 'bot_log_form';
+ }
+}
